e0b1ea228529ffcbffc247ced06e2fcf3018ca2e
markitus1996 <31216165+markitus1996@users.noreply.github.com>
2018-01-14 20:14:02 +0100

    eeeess

---
+++ mipaginaweb2 - copia/conexion.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>conexion</title>
+
+<body>
+
+<?php
+
+            $direccion = "localhost";
+            $nombrebbdd="proyecto2";
+            $usuario="root";
+            $contrasenya = "root";
+
+
+            $conexion = mysqli_connect ($direccion , $nombrebbdd , $usuario , $contrasenya);
+
+
+
+
+
+
+ ?>
+
+
+
+<body>
+</html>
